4e5448568268ab9076be95182554f1d653aeb7e0
verroichmawan <70270515+verroichmawan@users.noreply.github.com>
2020-08-26 19:45:53 +0700

    Add files via upload

---
+++ Biodata.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package biodata;
+
+/**
+ *
+ * @author USER
+ */
+public class Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nama="Verro Bhernadi Ichmawan";
+       String jk="laki-laki";
+       String alamat="Sumberporong";
+       String jurusan="XI RPL 1";
+       
+       System.out.println("biodata saya");//mengeprint atau menetak 
+       System.out.println("--------");
+       System.out.println("Nama\t\t\t:"+nama);
+       System.out.println("Jk\t\t\t:"+jk);
+       System.out.println("Alamat \t\t\t:"+alamat);
+       System.out.println("Jurusan\t\t:"+jurusan);
+    }
+    
+}
